502fb03ba1fa195a23b860e4aa09da87c0ee6bb0
Fabio <fasc@hotmail.de>
2018-06-22 21:59:09 +0200

    Added animation counter clock wise

--- MemeBoard/MainWindow.xaml.cs
+++ MemeBoard/MainWindow.xaml.cs
@@ -1,22 +1,12 @@
 ﻿using mrousavy;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfAnimatedGif;
 
 namespace MemeBoard
@@ -27,8 +17,9 @@ namespace MemeBoard
     public partial class MainWindow : Window
     {
         private Storyboard Storyboard => (Storyboard)this.Resources["imageRotationStoryboard"];
+        private Storyboard StoryboardCC => (Storyboard)this.Resources["imageRotationStoryboardCounterClock"];
 
-        private MemeRepo memeRepo = new MemeRepo(@"C:\Users\stream\Desktop\memes2");
+        private MemeRepo memeRepo = new MemeRepo(@"D:\Github\MemeBoard\Memes");
         private List<HotKey> keyBindings = new List<HotKey>();
 
         private Meme currentMeme = null;
@@ -47,6 +38,7 @@ namespace MemeBoard
             }
 
             Storyboard.Stop();
+            StoryboardCC.Stop();
             ImageBehavior.SetAnimatedSource(this.image, null);
 
             if (meme.IsAnimated)
@@ -79,11 +71,11 @@ namespace MemeBoard
         {
             this.keyBindings.ForEach(k => k.Dispose());
             this.keyBindings.Clear();
-            
+
             foreach (var meme in this.memeRepo.Memes)
             {
                 if (Enum.TryParse<Key>(meme.Prefix, true, out var result))
-                    this.keyBindings.Add(new HotKey(ModifierKeys.Control, 
+                    this.keyBindings.Add(new HotKey(ModifierKeys.Control,
                         result, this, _ => this.ToggleMeme(meme)));
             }
         }
@@ -95,7 +87,11 @@ namespace MemeBoard
             this.memeRepo.Updated += () => this.Dispatcher.Invoke(this.RefreshKeyBindings);
 
             new HotKey(ModifierKeys.Control, Key.PageUp, this, _ => this.Storyboard.Begin());
-            new HotKey(ModifierKeys.Control, Key.PageDown, this, _ => this.Storyboard.Stop());
+            new HotKey(ModifierKeys.Control, Key.PageDown, this, _ => this.StoryboardCC.Begin());
+            new HotKey(ModifierKeys.Control, Key.End, this, _ => {
+                this.Storyboard.Stop();
+                this.StoryboardCC.Stop();
+            });
         }
         
         private void TrayExit(object sender, RoutedEventArgs e)
